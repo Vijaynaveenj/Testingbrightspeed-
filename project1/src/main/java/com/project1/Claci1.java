2b9400bdb9a7d1734b55edcb2eaabc30caf191e6
VijayNaveen <VijayNaveen@Rev-PG02JZ5E>
2024-01-04 10:26:27 -0500

    Claci1 commit

---
+++ project1/src/main/java/com/project1/Claci1.java
@@ -0,0 +1,17 @@
+package com.project1;
+
+public class Claci1 {
+
+	public int addition(int i,int j) {
+		return i+j;
+	}
+	public int substration (int i,int j) {
+		return i-j;
+	}
+	public int multiplication(int i,int j) {
+		return i*j;
+	}
+	public int division(int i,int j) {
+		return i/j;
+	}
+}
