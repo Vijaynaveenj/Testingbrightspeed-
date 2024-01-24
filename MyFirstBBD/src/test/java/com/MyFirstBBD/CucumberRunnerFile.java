5c333a45248dfae8cf94fb9f0d3f3566c3237619
VijayNaveen <VijayNaveen@Rev-PG02JZ5E>
2024-01-24 12:15:46 -0500

    CommitBBD

---
+++ MyFirstBBD/src/test/java/com/MyFirstBBD/CucumberRunnerFile.java
@@ -0,0 +1,14 @@
+package com.MyFirstBBD;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+		features="src/test/resources/features"
+		)
+public class CucumberRunnerFile {
+
+}
--- project1/src/main/java/com/project1/Claci1.java
+++ project1/src/main/java/com/project1/Claci1.java
@@ -1,17 +1,22 @@
 package com.project1;
 
 public class Claci1 {
+	int a;
+	int b;
 
-	public int addition(int i,int j) {
-		return i+j;
+	public int addition(int i, int j) {
+		return i + j;
 	}
-	public int substration (int i,int j) {
-		return i-j;
+
+	public int substration(int i, int j) {
+		return i - j;
 	}
-	public int multiplication(int i,int j) {
-		return i*j;
+
+	public int multiplication(int i, int j) {
+		return i * j;
 	}
-	public int division(int i,int j) {
-		return i/j;
+
+	public int division(int i, int j) {
+		return i / j;
 	}
 }
